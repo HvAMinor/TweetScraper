107c4604a71438a28ba7f9287509eaf334c17efb
rudy.nutbeij <Rudy.Nutbeij@topicus.nl>
2019-03-28 14:02:07 +0100

    gefixt voor presentatie

--- TweetScraper/items.py
+++ TweetScraper/items.py
@@ -2,35 +2,49 @@
 
 # Define here the models for your scraped items
 from scrapy import Item, Field
-
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
 class Tweet(Item):
-    ID = Field()       # tweet id
-    url = Field()      # tweet url
-    datetime = Field() # post time
-    text = Field()     # text content
-    user_id = Field()  # user id
-    usernameTweet = Field() # username of tweet
+    ID = Field(
+        output_processor=TakeFirst()
+    )
+    user_id = Field(
+        output_processor=TakeFirst()
+    )
+    text = Field(
+        input_processor = MapCompose(remove_tags),
+        output_processor = Join()
+    )
+    url = Field(
+        output_processor = TakeFirst()
+    )
+    timestamp = Field(
+        output_processor = TakeFirst()
+    )
+    retweets = Field(
+        output_processor = TakeFirst()
+    )
+    favorites = Field(
+        output_processor = TakeFirst()
+    )
+    replies = Field(
+        output_processor = TakeFirst()
+    )
+    lang = Field()
 
-    nbr_retweet = Field()  # nbr of retweet
-    nbr_favorite = Field() # nbr of favorite
-    nbr_reply = Field()    # nbr of reply
-
-    is_reply = Field()   # boolean if the tweet is a reply or not
-    is_retweet = Field() # boolean if the tweet is just a retweet of another tweet
-
-    has_image = Field() # True/False, whether a tweet contains images
-    images = Field()    # a list of image urls, empty if none
-
-    has_video = Field() # True/False, whether a tweet contains videos
-    videos = Field()    # a list of video urls
-
-    has_media = Field() # True/False, whether a tweet contains media (e.g. summary)
-    medias = Field()    # a list of media
+    conversation_id = Field(
+        output_processor = TakeFirst()
+    )
 
 
 class User(Item):
-    ID = Field()            # user id
-    name = Field()          # user name
-    screen_name = Field()   # user screen name
-    avatar = Field()        # avator url
+    ID = Field(
+        output_processor=TakeFirst()
+    )
+    screenname = Field(
+        output_processor=TakeFirst()
+    )
+    name = Field(
+        output_processor=TakeFirst()
+    )
--- TweetScraper/settings.py
+++ TweetScraper/settings.py
@@ -12,19 +12,8 @@ SPIDER_MODULES = ['TweetScraper.spiders']
 NEWSPIDER_MODULE = 'TweetScraper.spiders'
 ITEM_PIPELINES = {
     'TweetScraper.pipelines.SaveToFilePipeline':100,
-    #'TweetScraper.pipelines.SaveToMongoPipeline':100, # replace `SaveToFilePipeline` with this to use MongoDB
-    #'TweetScraper.pipelines.SavetoMySQLPipeline':100, # replace `SaveToFilePipeline` with this to use MySQL
 }
 
 # settings for where to save data on disk
 SAVE_TWEET_PATH = './Data/tweet/'
 SAVE_USER_PATH = './Data/user/'
-
-# settings for mongodb
-MONGODB_SERVER = "127.0.0.1"
-MONGODB_PORT = 27017
-MONGODB_DB = "TweetScraper"        # database name to save the crawled data
-MONGODB_TWEET_COLLECTION = "tweet" # collection name to save tweets
-MONGODB_USER_COLLECTION = "user"   # collection name to save users
-
-
